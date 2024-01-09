8c8b172d3173d765c5b5f76ba73e9b6f19215583
khalidelaasry <khalidelaasry22@gmail.com>
2024-01-09 21:52:51 +0100

    carryOut.c

--- carryOut.c
+++ carryOut.c
@@ -1,25 +1,31 @@
 #include "shell.h"
 
-int carryOut(char **comd,char **argv)
+/**
+ * carryOut - Executes a command with arguments.
+ * @comd: Array containing the command and its arguments.
+ * @argv: Array of strings representing the program's command-line arguments.
+ *
+ * Return: The exit status of the executed command.
+ */
+int	carryOut(char **comd, char **argv)
 {
-   /* char *full_cmd;*/
-    pid_t child;
-    int statusFlag;
+	pid_t child;
+	int statusFlag;
 
-    child = fork();
-    if(child == 0)
-    {
-       if (execve(comd[0], comd, environ) == -1)
-        {
-           perror(argv[0]);
-            freearv(comd);
-            exit(0);
-        }
-    }
-    else
-    {
-        waitpid(child,&statusFlag,0);
-       freearv(comd);
-    }
-    return (WEXITSTATUS(statusFlag));
+	child = fork();
+	if (child == 0)
+	{
+		if (execve(comd[0], comd, environ) == -1)
+		{
+			perror(argv[0]);
+			freearv(comd);
+			exit(0);
+		}
+	}
+	else
+	{
+		waitpid(child, &statusFlag, 0);
+		freearv(comd);
+	}
+	return (WEXITSTATUS(statusFlag));
 }
