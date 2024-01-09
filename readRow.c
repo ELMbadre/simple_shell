30dc616a9ed3da96ca1410265b17cb5536d445b6
khalidelaasry <khalidelaasry22@gmail.com>
2024-01-09 22:09:47 +0100

    readRow.c

--- readRow.c
+++ readRow.c
@@ -1,18 +1,23 @@
 #include "shell.h"
 
-char *readRow(void)
+/**
+ * readRow - Reads a line of input from the user.
+ *
+ * Return: A pointer to the read line on success, or NULL on failure.
+ */
+char	*readRow(void)
 {
-        char *ln = NULL;
-        size_t len = 0;
-        ssize_t i;
+	char *ln = NULL;
+	size_t len = 0;
+	ssize_t i;
 
-        if (isatty(STDIN_FILENO))
-                write(STDOUT_FILENO, "$ ", 2);
-        i = getline(&ln, &len, stdin);
-        if (i == -1)
-        {
-                free(ln);
-                return (NULL);
-        }
-        return (ln); 
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "$ ", 2);
+	i = getline(&ln, &len, stdin);
+	if (i == -1)
+	{
+		free(ln);
+		return (NULL);
+	}
+	return (ln);
 }
