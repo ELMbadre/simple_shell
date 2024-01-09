8ea3e5f27929d0143b46ff118c1359d206ace992
khalidelaasry <khalidelaasry22@gmail.com>
2024-01-09 22:04:16 +0100

    freearv.c

--- freearv.c
+++ freearv.c
@@ -1,14 +1,19 @@
 #include "shell.h"
 
-void freearv(char **aray)
+/**
+ * freearv - Frees memory allocated for an array of strings.
+ * @aray: Array of strings to be freed.
+ */
+void	freearv(char **aray)
 {
-    int n;
-    if (!aray)
-        return;
-    for (n = 0 ;aray[n]; n++)
-    {
-        free(aray[n]);
-        aray[n] = NULL;
-    }
-        free(aray), aray = NULL;
+	int n;
+
+	if (!aray)
+		return;
+	for (n = 0 ; aray[n]; n++)
+	{
+		free(aray[n]);
+		aray[n] = NULL;
+	}
+	free(aray), aray = NULL;
 }
