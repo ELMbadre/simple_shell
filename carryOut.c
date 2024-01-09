26482d99a8f328f81944f60b3f729dc9b24ace13
khalidelaasry <khalidelaasry22@gmail.com>
2024-01-10 00:58:48 +0100

    carryOut.c

--- carryOut.c
+++ carryOut.c
@@ -1,31 +1,43 @@
 #include "shell.h"
 
 /**
- * carryOut - Executes a command with arguments.
- * @comd: Array containing the command and its arguments.
- * @argv: Array of strings representing the program's command-line arguments.
+ * carryOut - Execute a command and wait for its completion.
+ * @comd: An array of strings representing the command and its arguments.
+ * @argv: An array of strings representing theprogram's command-linearguments.
  *
- * Return: The exit status of the executed command.
+ * This function forks a child process, executes the specified command using
+ * execve, and waits for the child process to complete. It returns the exit
+ * status of the child process.
+ *
+ * Return: The exit status of the child process.
  */
-int	carryOut(char **comd, char **argv)
+int	carryout(char **comd, char **argv, int idx)
 {
+	 char *full_cmd;
 	pid_t child;
 	int statusFlag;
 
+	full_cmd = fetchpath(comd[0]);
+	if (!full_cmd)
+	{
+		printerror(argv[0], comd[0], idx);
+		freearv(comd);
+		return (127);
+	}
 	child = fork();
 	if (child == 0)
 	{
-		if (execve(comd[0], comd, environ) == -1)
+		if (execve(full_cmd, comd, environ) == -1)
 		{
-			perror(argv[0]);
+			free(full_cmd), full_cmd = NULL;
 			freearv(comd);
-			exit(0);
 		}
 	}
 	else
 	{
 		waitpid(child, &statusFlag, 0);
 		freearv(comd);
+		 free(full_cmd), full_cmd = NULL;
 	}
 	return (WEXITSTATUS(statusFlag));
 }
