5476ebb56bbc93308558a7b1ece78a83c401807f
khalidelaasry <khalidelaasry22@gmail.com>
2024-01-09 22:32:35 +0100

    main.c

--- main.c
+++ main.c
@@ -1,35 +1,36 @@
 #include "shell.h"
 
-/*
- * main - Simple Shell main function
- * @ac : Count of argument
- * @av : Arguments
- * Return : 0 Always (success)
+/**
+ * main - The main function of the shell.
+ * @ac: The number of command-line arguments.
+ * @av: An array containing the command-line arguments.
+ *
+ * Return: The exit status of the shell.
  */
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    char *ln = NULL;
-    char **cmd = NULL;
-    int stateInfo = 0 /*idx = 0*/;
-    (void)ac;
-    (void)av;
+	char *ln = NULL;
+	char **cmd = NULL;
+	int stateInfo = 0;
+	(void)ac;
+	(void)av;
 
-    while (1)
-    {
-        ln = readRow();
-        if (ln == NULL)
-        {
-            if (isatty(STDIN_FILENO))
-                write(STDOUT_FILENO, "\n", 1);
-            return (stateInfo);
-        }
-        /*  idx++;*/
-        cmd = textSegmenter(ln);
-        if (!cmd)
-            continue;
-    /*if (is_builtin(cmd[0]))
-            handle_builtin(cmd, av, &stateInfo, idx);*/
-        /*else*/
-          stateInfo = carryOut(cmd, av);
-    }
+	while (1)
+	{
+		ln = readRow();
+		if (ln == NULL)
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			return (stateInfo);
+		}
+		/*  idx++;*/
+		cmd = textSegmenter(ln);
+		if (!cmd)
+			continue;
+		/*if (is_builtin(cmd[0]))*/
+		 /* handle_builtin(cmd, av, &stateInfo, idx);*/
+		/*else*/
+		stateInfo = carryOut(cmd, av);
+	}
 }
